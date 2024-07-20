e93b95a080b97fd9d382c9e775042a0279b96138
NuraliAKH <axmadovnurali5@gmail.com>
2024-07-20 16:41:16 +0500

    loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const LoadingSpinner: React.FC = () => {
+  const spinnerStyle: React.CSSProperties = {
+    border: "8px solid #f3f3f3",
+    borderTop: "8px solid #3498db",
+    borderRadius: "50%",
+    width: "60px",
+    height: "60px",
+    animation: "spin 2s linear infinite",
+  };
+
+  const containerStyle: React.CSSProperties = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  };
+
+  return (
+    <div style={containerStyle}>
+      <style>
+        {`
+          @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }
+        `}
+      </style>
+      <div style={spinnerStyle}></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
